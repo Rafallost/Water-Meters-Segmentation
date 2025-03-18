408e835271177e6473d87887f68d5cf28afe69d2
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-03-18 21:08:13 +0100

    Update dataset.py

--- WMS/src/dataset.py
+++ WMS/src/dataset.py
@@ -7,10 +7,11 @@ import cv2
 """
 
 class WMSDataset(Dataset):
-    def __init__(self, imagePaths, maskPaths, transforms):
+    def __init__(self, imagePaths, maskPaths, imageTransforms, maskTransforms,):
         self.imagePaths = imagePaths
         self.maskPaths = maskPaths
-        self.transforms = transforms
+        self.imageTransforms = imageTransforms
+        self.maskTransforms = maskTransforms
 
     def __len__(self):
         return len(self.imagePaths)
@@ -18,15 +19,14 @@ class WMSDataset(Dataset):
     def __getitem__(self, i):
         image_path = self.imagePaths[i] #Grab the image path form the current index
         image = cv2.imread(image_path) # Load image
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # Swap channels from BGR to RGB
 
         mask_path = self.maskPaths[i]
         mask = cv2.imread(mask_path, 0) # 0 - greyscale
 
-        # Normalization
-        if self.transforms is not None:
-            image = self.transforms(image)
-            mask = self.transforms(mask)
+        if self.imageTransforms:
+            image = self.imageTransforms(image)
+        if self.maskTransforms:
+            mask = self.maskTransforms(mask)
 
         return image, mask
 
