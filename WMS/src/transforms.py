92570e31efde71dccc2fd3353a0e9afa0c85a2a5
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-03-18 21:07:39 +0100

    Add transforms file

---
+++ WMS/src/transforms.py
@@ -0,0 +1,20 @@
+import torchvision.transforms as TRANS
+
+imageTransforms = TRANS.Compose([
+    TRANS.ToPILImage(),
+    TRANS.Resize((256, 256)),
+    TRANS.RandomHorizontalFlip(p=0.5),  # 50% chance to reflect
+    TRANS.RandomRotation(15),  # max 15 degres rotation
+    TRANS.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1), # change colors
+    TRANS.ToTensor()
+    #TRANS.Normalize(mean=[0.5], std=[0.5])  # Normalization to [-1,1]
+])
+
+# Masks contain classes, not pixel values - without normalization
+maskTransforms = TRANS.Compose([
+    TRANS.ToPILImage(),
+    TRANS.Resize((256, 256)),
+    TRANS.RandomHorizontalFlip(p=0.5),
+    TRANS.RandomRotation(15),
+    TRANS.ToTensor()
+])
