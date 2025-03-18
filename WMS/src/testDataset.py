cbe90d924ccabbf738404d1d04cddcecbf057f72
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-03-18 21:08:33 +0100

    Add logs

--- WMS/src/testDataset.py
+++ WMS/src/testDataset.py
@@ -1,8 +1,10 @@
-from random import random
-
-from dataset import WMSDataset
 import os
-import cv2
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+from dataset import WMSDataset
+from transforms import imageTransforms, maskTransforms
 
 image_dir = r"D:\Github\Water-Meters-Segmentation\WMS\data\images"
 mask_dir = r"D:\Github\Water-Meters-Segmentation\WMS\data\masks"
@@ -10,14 +12,29 @@ mask_dir = r"D:\Github\Water-Meters-Segmentation\WMS\data\masks"
 imagePaths = [os.path.join(image_dir, f) for f in os.listdir(image_dir)]
 maskPaths = [os.path.join(mask_dir, f) for f in os.listdir(mask_dir)]
 
-transforms = None  # torchvision.transforms, to tensor
+dataset = WMSDataset(imagePaths, maskPaths, imageTransforms, maskTransforms)
 
-dataset = WMSDataset(imagePaths, maskPaths, transforms)
+print(f"Dataset length: {len(dataset)}")
 
-image, mask = dataset[0]
+dataLoader = DataLoader(dataset, batch_size=5, shuffle=True)
 
-cv2.imshow("Image", image)
-cv2.imshow("Mask", mask)
+images, masks = next(iter(dataLoader))
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+fig, axs = plt.subplots(5, 2, figsize=(10, 20))
+for i in range(5):
+    image = images[i].permute(1, 2, 0).numpy()
+    mask = masks[i].squeeze().numpy()
+    axs[i, 0].imshow(image)
+    axs[i, 0].set_title("Obraz")
+    axs[i, 1].imshow(mask, cmap='gray')
+    axs[i, 1].set_title("Maska")
+    axs[i, 0].axis("off")
+    axs[i, 1].axis("off")
+
+
+print(f"PyTorch version: {torch.__version__}")
+print(f"Torchvision version: {torchvision.__version__}")
+print(f"GPU available: {torch.cuda.is_available()}")
+
+plt.show()
+
