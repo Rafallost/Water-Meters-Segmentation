62c74efbe3d70079e6e3b70b58ce8f2a18df1a6b
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-03-20 22:15:20 +0100

    Add Decoder class

--- WMS/src/model.py
+++ WMS/src/model.py
@@ -35,3 +35,24 @@ class Encoder(Module):
 
         return blockOutputs
 
+class Decoder(Module):
+	def __init__(self, channels=(64, 32, 16)):
+		super().__init__()
+		self.channels = channels
+		self.upconvs = ModuleList(
+			[ConvTranspose2d(channels[i], channels[i + 1], 2, 2)
+			 	for i in range(len(channels) - 1)])
+		self.dec_blocks = ModuleList(
+			[Block(channels[i], channels[i + 1])
+			 	for i in range(len(channels) - 1)])
+	def forward(self, x, encFeatures):
+		for i in range(len(self.channels) - 1):
+			x = self.upconvs[i](x)
+			encFeat = self.crop(encFeatures[i], x)
+			x = torch.cat([x, encFeat], dim=1)
+			x = self.dec_blocks[i](x)
+		return x
+	def crop(self, encFeatures, x):
+		(_, _, H, W) = x.shape
+		encFeatures = CenterCrop([H, W])(encFeatures)
+		return encFeatures
