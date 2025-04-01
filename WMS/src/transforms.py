c1e3cc13f10783bdf5953be981d00acfb6878615
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-04-01 20:12:10 +0200

    Resize parameters to 512x512

--- WMS/src/transforms.py
+++ WMS/src/transforms.py
@@ -2,17 +2,13 @@ import torchvision.transforms as TRANS
 
 imageTransforms = TRANS.Compose([
     TRANS.ToPILImage(),
-    TRANS.Resize((256, 256)),
-    #TRANS.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1), # change colors
+    TRANS.Resize((512, 512)), #512/4        768/2
     TRANS.ToTensor()
-    #TRANS.Normalize(mean=[0.5], std=[0.5])  # Normalization to [-1,1]
 ])
 
 # Masks contain classes, not pixel values - without normalization
 maskTransforms = TRANS.Compose([
     TRANS.ToPILImage(),
-    TRANS.Resize((256, 256)),
-    #TRANS.RandomHorizontalFlip(p=0.5),
-    #TRANS.RandomRotation(15),
+    TRANS.Resize((512, 512)), #512/4        768/2
     TRANS.ToTensor()
 ])
