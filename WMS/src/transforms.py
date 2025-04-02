3f38454d88e851d994f67c60c800f4982b85543e
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-04-02 10:41:32 +0200

    Add normalization

--- WMS/src/transforms.py
+++ WMS/src/transforms.py
@@ -3,7 +3,8 @@ import torchvision.transforms as TRANS
 imageTransforms = TRANS.Compose([
     TRANS.ToPILImage(),
     TRANS.Resize((512, 512)), #512/4        768/2
-    TRANS.ToTensor()
+    TRANS.ToTensor(),
+    TRANS.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 ])
 
 # Masks contain classes, not pixel values - without normalization
