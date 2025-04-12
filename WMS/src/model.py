3d972f44afee310099243a5909ba030a4151ae8d
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-04-12 12:56:30 +0200

    Rework model (decode)

--- WMS/src/model.py
+++ WMS/src/model.py
@@ -8,15 +8,15 @@ class WaterMetersUNet(nn.Module):
 
         # ENCODER
         self.enc1 = nn.Sequential(
-            nn.Conv2d(inChannels, baseFilters, kernel_size=3, padding=1),
+            nn.Conv2d(inChannels, baseFilters, kernel_size=3, padding=1), # inChannels -> baseChannels (3 -> 16)
             nn.BatchNorm2d(baseFilters),
             nn.ReLU(inplace=True)
         )
-        self.pool1 = nn.MaxPool2d(2, stride=2) # Resize h, w by half (e.g. 512->256)
+        self.pool1 = nn.MaxPool2d(2, stride=2) # Resize h, w by half (e.g. 512 -> 256)
 
 
         self.enc2 = nn.Sequential(
-            nn.Conv2d(baseFilters, baseFilters * 2, kernel_size=3, padding=1),
+            nn.Conv2d(baseFilters, baseFilters * 2, kernel_size=3, padding=1), # (16 -> 32)
             nn.BatchNorm2d(baseFilters * 2),
             nn.ReLU(inplace=True)
         )
@@ -24,7 +24,7 @@ class WaterMetersUNet(nn.Module):
 
 
         self.enc3 = nn.Sequential(
-            nn.Conv2d(baseFilters * 2, baseFilters * 4, kernel_size=3, padding=1),
+            nn.Conv2d(baseFilters * 2, baseFilters * 4, kernel_size=3, padding=1), # (32 -> 64)
             nn.BatchNorm2d(baseFilters * 4),
             nn.ReLU(inplace=True)
         )
@@ -32,8 +32,31 @@ class WaterMetersUNet(nn.Module):
 
         # Deepest layer of UNet, without pooling after that
         self.bottleneck = nn.Sequential(
-            nn.Conv2d(baseFilters * 4, baseFilters * 8, kernel_size=3, padding=1),
+            nn.Conv2d(baseFilters * 4, baseFilters * 8, kernel_size=3, padding=1), # (64 -> 128)
             nn.BatchNorm2d(baseFilters * 8),
             nn.ReLU(inplace=True)
         )
+
+
+        # DECODER (Rebuilding resolution (upsampling))
+        self.dec3 = nn.Sequential( # bottleneck + enc3 -> (128 + 64 = 192 [channels])
+            nn.Conv2d(baseFilters * 8 + baseFilters * 4 ,baseFilters * 4, kernel_size=3, padding=1), # 192 -> 64
+            nn.BatchNorm2d(baseFilters * 4),
+            nn.ReLU(inplace=True)
+        )
+
+        self.dec2 = nn.Sequential( # dec3 + enc2 -> (64 + 32 = 96 [ channels])
+            nn.Conv2d(baseFilters * 4 + baseFilters * 2, baseFilters * 2, kernel_size=3, padding=1), # 96 -> 32
+            nn.BatchNorm2d(baseFilters * 2),
+            nn.ReLU(inplace=True)
+        )
+
+        self.dec1 = nn.Sequential( # dec2 + enc1 -> (32 + 16 = 48 [channels])
+            nn.Conv2d(baseFilters * 2 + baseFilters, baseFilters, kernel_size=3, padding=1), # 48 -> 16
+            nn.BatchNorm2d(baseFilters),
+            nn.ReLU(inplace=True)
+        )
+
+        # Last layer 1x1 16 -> outChannels (1)
+        self.final = nn.Conv2d(baseFilters, outChannels, kernel_size=1)
 
