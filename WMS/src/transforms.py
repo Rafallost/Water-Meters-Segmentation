3a02de1313864e702b47351b464ca14192fead0d
Rafallost <rafalzab20@gmail.com>
2025-04-23 05:36:20 +0200

    Change name testDataset.py -> prepareDataset.py

--- WMS/src/transforms.py
+++ WMS/src/transforms.py
@@ -4,7 +4,7 @@ imageTransforms = TRANS.Compose([
     TRANS.ToPILImage(),
     TRANS.Resize((512, 512)), #512/4        768/2
     TRANS.ToTensor(),
-    TRANS.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    # TRANS.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 ])
 
 # Masks contain classes, not pixel values - without normalization
